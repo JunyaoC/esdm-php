cada7fe71f4b100b8fbb9490f502fda35135e1a0
rouxuen <rouxuen0322@hotmail.com>
2021-05-21 23:53:39 +0800

    update history

---
+++ php-folder/procourse/history.php
@@ -0,0 +1,35 @@
+<?php   
+
+    include "../connect.php";
+
+    $postjson = json_decode(file_get_contents('php://input'), true);
+
+    
+    if($postjson['action'] == 'list_history') {
+
+
+        $query = mysqli_query($conn, "SELECT rh.*,s.*,c.*,sc.* FROM tb_procourse_regHistory rh 
+        LEFT JOIN tb_procourse_section sc ON rh.procourse_sec=sc.courseSec_id
+        LEFT JOIN tb_pro_course c ON sc.courseSec_courseID=c.procourse_code
+        LEFT JOIN tb_student s ON rh.stu_id=s.student_matric");
+
+        $read_data = array();
+
+        while($read = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+            $data = array(
+                'procourse_code' => $read['procourse_code'],
+                'procourse_name' => $read['procourse_name'],
+                'courseSec_date' => $read['courseSec_date'],          
+            );
+            array_push($read_data,$data);
+        }
+
+        $result = json_encode(array('success'=>true, 'msg'=>'success', 'history'=>$read_data));
+
+        echo $result;
+
+    }
+
+
+
+?>
