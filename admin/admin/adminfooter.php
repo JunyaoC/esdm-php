33483e41795afe7bef5e241bb5cab737324ac86a
Khoo Jie Xuan <coolkhoo547@gmail.com>
2021-05-22 01:45:58 +0800

    PHP Admin Panel

---
+++ admin/admin/adminfooter.php
@@ -0,0 +1,17 @@
+  <footer class="footer footer-black  footer-white ">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="credits ml-auto">
+             
+            <img src="../img/UTM.png" width="30" height="25" >
+        
+              <span class="copyright">
+                ©
+                <script>
+                  document.write(new Date().getFullYear())
+                </script>. Developed by ESDM Section 01 2020/2021
+              </span>
+            </div>
+          </div>
+        </div>
+      </footer>
---
+++ admin/login-styling.php
@@ -0,0 +1,31 @@
+  <title>UTM ESDM System</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="img/logo.png" rel="shortcut icon"/>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  
+  <!-- Google font -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+ 
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="css/bootstrap.min.css"/>
+  <link rel="stylesheet" href="css/font-awesome.min.css"/>
+  <link rel="stylesheet" href="css/owl.carousel.min.css"/>
+  <link rel="stylesheet" href="css/slicknav.min.css"/>
+
+  <!-- Main Stylesheets -->
+  <link rel="stylesheet" href="css/style.css"/>
+  <link rel="stylesheet" href="css/loginstyle.css">
+  <style type="text/css">
+  .error {
+    width: 100%; 
+    margin: 0px auto; 
+    padding: 10px; 
+    border: 1px solid #a94442; 
+    color: #a94442; 
+    background: #f2dede; 
+    border-radius: 5px; 
+    text-align: left;
+  }
+ </style>
