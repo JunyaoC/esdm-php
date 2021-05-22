8ccb84876c0ba59160e3d8edf03240a3f90456c4
rouxuen <rouxuen0322@hotmail.com>
2021-05-22 13:19:52 +0800

    update details

--- php-folder/procourse/coursedetails.php
+++ php-folder/procourse/coursedetails.php
@@ -29,6 +29,33 @@
 
     }
 
+    if($postjson['action'] == 'list_section') {
+
+
+        $query = mysqli_query($conn, "SELECT c.*,sc.* FROM tb_pro_course c 
+        LEFT JOIN tb_procourse_section sc  ON sc.courseSec_courseID=c.procourse_code
+        WHERE sc.courseSec_courseID='$id'");
+
+        $read_data = array();
+       
+            while($read = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+            $data = array(
+                'courseSec_id' => $read['courseSec_id'],
+                'courseSec_seat' => $read['courseSec_seat'],
+                'courseSec_maxseat' => $read['courseSec_maxseat'],
+            );
+            array_push($read_data,$data);
+             }
+             $result = json_encode(array('success'=>true, 'msg'=>'success', 'section'=>$read_data));
+
+            echo $result;
+        
+        
+
+        
+
+    }
+
 
 
 ?>
