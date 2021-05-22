442f43ea32b301cf2237491cd39a0d6dbb5de369
Jiakmai <71082196+Jiakmai@users.noreply.github.com>
2021-05-22 18:41:50 +0800

    Update open-registration.php

--- php-folder/hostel/open-registration.php
+++ php-folder/hostel/open-registration.php
@@ -1,22 +1,24 @@
 <?php 
 
-	include "./connect.php";
+	include "../connect.php";
 	$postjson = json_decode(file_get_contents('php://input'), true);
 
-	if($postjson['action'] == 'list_college') {
-		$u_id = $postjson['u_id'];
+	// if($postjson['action'] == 'list_college') {
+		
 		$query = mysqli_query($conn, "SELECT * FROM tb_college");
-
 		$read_college = array();
 
 		while($read = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
 			$data = array(
-				 'kolej_id'=> $read['kolej_id'];
-				'kolej_name' => $read['kolej_name'];
+				 'kolej_id'=> $read['kolej_id'],
+				'kolej_name' => $read['kolej_name'],
+
 			);
 	            array_push($read_college,$data);
+	            echo $data[0];
 	        }
-	        $result = json_encode(array('success'=>true,'msg'=>'success','colleges'=>$read_college))
 
-	        echo $result;}
+	        $result = json_encode(array('success'=>true,'msg'=>'success','colleges'=>$read_college));
+
+	    // }
 ?>
