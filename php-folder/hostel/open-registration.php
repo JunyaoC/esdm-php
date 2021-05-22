7f48f23e2a77f8e8f691b852cd32d5a98df490c5
Jiakmai <71082196+Jiakmai@users.noreply.github.com>
2021-05-22 21:36:30 +0800

    open-reg

--- php-folder/hostel/open-registration.php
+++ php-folder/hostel/open-registration.php
@@ -4,7 +4,7 @@
 	$postjson = json_decode(file_get_contents('php://input'), true);
 
 	if($postjson['action'] == 'list_college') {
-		
+
 		$query = mysqli_query($conn, "SELECT * FROM tb_college");
 		$read_college = array();
 
@@ -18,6 +18,18 @@
 	        }
 
 	        $result = json_encode(array('success'=>true,'msg'=>'success','colleges'=>$read_college));
+	        echo $result;
+	    }
+
+	    if($postjson['action'] == 'select_college'){
+	    	$insert = $mysqli_query ($conn,"INSERT INTO tb_hostel_reg SET hostel_id = '$postjson[kolej_id]'");
+	    	
+	    	if($insert){
+	    		$result = json_encode(array('success'=>true,'msg'=>'success'));
+	    	}else{
+				$result = json_encode(array('success'=>false,'msg'=>'fail'));
+	    	}
+	    echo $result;
 
 	    }
 ?>
