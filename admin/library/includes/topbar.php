873e47730a5438a418cd8aec28d5868bf79e94c7
vallawxy <66740756+vallawxy@users.noreply.github.com>
2021-05-23 04:00:04 +0800

    topbar

--- admin/library/includes/topbar.php
+++ admin/library/includes/topbar.php
@@ -29,7 +29,7 @@
                 			<ul class="nav navbar-nav navbar-right" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
                              
                 				
-                				    <li><a href="logout.php" class="color-danger text-center"><i class="fa fa-sign-out"></i> Log Keluar</a></li>
+                				    <li><a href="../admin/dashboard.php" class="color-danger text-center"><i class="fa fa-sign-out"></i> Back to Admin</a></li>
                 					
                 		
                             
