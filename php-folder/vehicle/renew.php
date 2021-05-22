2c157aa100cec1edb63dcd4f0f594037a48ca6c3
Kathelene Yap <katheleneyap@gmail.com>
2021-05-22 22:53:05 +0800

    Update renew.php

--- php-folder/vehicle/renew.php
+++ php-folder/vehicle/renew.php
@@ -2,23 +2,37 @@
 include "../connect.php";
 
 $postjson = json_decode(file_get_contents('php://input'),true);
+$stuACID = $postjson['stuACID'];
+$stickerID = $postjson['stickerID'];
+$vehicleID=$postjson['vehicleID'];
+$paymentAmount = $postjson['paymentAmount'];
+$paymentProve = $postjson['paymentProve'];
+$paymentStatus = $postjson['paymentStatus'];
+$today = date('Y-m-d');
 
-if($postjson['action']=='show_record'){
-    $query = mysqli_query($conn,"SELECT * FROM tb_sticker LEFT JOIN tb_vehicle ON tb_sticker.vehiclePlateNo=tb_vehicle.vehicleID");
-    $read_data = array();
-    while($read=mysqli_fetch_array($query, MYSQLI_ASSOC)){
-        $data=array(
-            'id'=> $read['stickerID'],
-            'vehiclePlateNo'=> $read['vehiclePlateNo'],
-            'vehicleModel'=> $read['vehicleModel'],
-            'vehicleColor'=> $read['vehicleColor'],
-            'vehicleType'=> $read['vehicleType'],
-        );
-        array_push($read_data,$data);
+if($postjson['action']=='renew_sticker'){
+    //Check if the sticker exist
+    $exist_check = mysqli_query($conn, "SELECT * FROM tb_sticker WHERE stickerID = '$stickerID' AND stickerStatus='Rejected'");
+
+	$row_cnt = mysqli_num_rows($exist_check);
+    
+    if($row_cnt > 0){
+        $insert = mysqli_query($conn, "INSERT INTO tb_payment (stuACID, stickerID, paymentAmount,paymentDate,paymentProve,paymentStatus) 
+    VALUES('$stuACID',(SELECT stickerID FROM tb_sticker WHERE tb_sticker.vehiclePlateNo = '$vehicleID'),'$paymentAmount','$today','$paymentProve','$paymentStatus')");
+	
+		if($insert) {
+			$result = json_encode(array('success'=>true, 'msg'=>'success'));
+		} else {
+			$result = json_encode(array('success'=>false, 'msg'=>'fail'));
+		}
+	
+		echo $result;
     }
-    $result = json_encode(array('success'=>true, 'msg'=>'success','record'=>$read_data));
-
-    echo $result;
+    else
+    {
+        $result = json_encode(array('success'=>false, 'msg'=>'Sticker does not exist or still valid'));
+        echo $result;
+    }
 }
 //echo "OK";
 //echo "Theresa";
