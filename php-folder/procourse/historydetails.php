8487a3bd7d12879fbe31794f771ba8879951cad1
rouxuen <rouxuen0322@hotmail.com>
2021-05-22 23:39:22 +0800

    update list

--- php-folder/procourse/historydetails.php
+++ php-folder/procourse/historydetails.php
@@ -3,7 +3,7 @@
     include "../connect.php";
 
     $postjson = json_decode(file_get_contents('php://input'), true);
-
+    $today=date('Y-m-d');
     $id=$postjson['id'];
     if($postjson['action'] == 'list_section') {
 
@@ -12,12 +12,16 @@
         LEFT JOIN tb_procourse_section cs ON rh.procourse_sec=cs.courseSec_id
         LEFT JOIN tb_pro_course c ON cs.courseSec_courseID=c.procourse_code
         LEFT JOIN tb_procourse_fac f ON cs.courseSec_fac=f.fac_id
-        WHERE rh.procourse_sec='$id'
+        WHERE rh.regHis_id='$id'
         ");
 
         $read_data = array();
 
         while($read = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+            $date=$read['courseSec_date'];
+            $diff=(strtotime($date) - strtotime($today)) / (60 * 60 * 24);
+
+
             $data = array(
                 'procourse_name' => $read['procourse_name'],
                 'procourse_code' => $read['procourse_code'],
@@ -25,7 +29,12 @@
                 'courseSec_date' => $read['courseSec_date'],
                 'courseSec_loc' => $read['courseSec_loc'],
                 'fac_name' => $read['fac_name'],
-                'section_no' => $read['section_no'],       
+                'section_no' => $read['section_no'],
+                'courseSec_seat' => $read['courseSec_seat'],
+                'courseSec_date' => $read['courseSec_date'],
+                'regHis_id' => $read['regHis_id'],
+                'procourse_sec' => $read['procourse_sec'],
+                'diff' => $diff,   
             );
             array_push($read_data,$data);
         }
@@ -39,21 +48,21 @@
 
     if($postjson['action'] == 'cancel') {
         $seat=$postjson['seat'];
-        $sec_id=$postjson['course_section'];
+        $sec_id=$postjson['coursesec_id'];
 
-		$insert = mysqli_query($conn, "INSERT INTO tb_procourse_regHistory SET stu_id = '$postjson[student]', procourse_sec = '$postjson[course_section]'");
+		$delete = mysqli_query($conn, "DELETE FROM tb_procourse_regHistory WHERE regHis_id = '$postjson[regHis_id]'");
         $update = mysqli_query($conn, "UPDATE tb_procourse_section SET courseSec_seat = $seat WHERE courseSec_id =$sec_id");
 
 		if($insert) {
-			$result = json_encode(array('success'=>true, 'msg'=>'Success to register'));
+			$result = json_encode(array('success'=>true, 'msg'=>'Success to cancel'));
 		} else {
-			$result = json_encode(array('success'=>false, 'msg'=>'Fail to register'));
+			$result = json_encode(array('success'=>false, 'msg'=>'Fail to cancel'));
 		}
 
         if($update) {
-			$result1 = json_encode(array('success'=>true, 'msg'=>'Success to key in'));
+			$result1 = json_encode(array('success'=>true, 'msg'=>'Success to update'));
 		} else {
-			$result1 = json_encode(array('success'=>false, 'msg'=>'Fail to key in'));
+			$result1 = json_encode(array('success'=>false, 'msg'=>'Fail to update'));
 		}
 
 		echo $result;
