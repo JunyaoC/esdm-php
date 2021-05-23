babf7b57cdea69e0c6872fdf152148a6ced0006a
rouxuen <rouxuen0322@hotmail.com>
2021-05-23 13:48:37 +0800

    update

--- php-folder/procourse/reportissue.php
+++ php-folder/procourse/reportissue.php
@@ -1,16 +1,10 @@
 <?php
 	
-	header('Access-Control-Allow-Origin: *');
-	header('Access-Control-Allow-Credentials: true');
-	header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
-	header("Access-Control-Allow-Headers: Origin, Content-Type, Authorization, Accept, X-Requested-With, x-xsrf-token");
-	header("Content-Type: application/json; charset=utf-8");
-
 	include "../connect.php";
 
 	$postjson = json_decode(file_get_contents('php://input'), true);
 	$today = date('Y-m-d');
-
+    
 	if($postjson['action'] == 'create_issue') {
 
 		$insert = mysqli_query($conn, "INSERT INTO tb_procourse_issue SET stu_matric = '$postjson[matric]', issue_title = '$postjson[title]', issue_details = '$postjson[content]', issue_date = '$today', issue_status = '0'");
@@ -24,9 +18,9 @@
 		echo $result;
 
 	}elseif($postjson['action'] == 'list_issue') {
+        $stu_matric=$postjson['student'];
 
-
-        $query = mysqli_query($conn, "SELECT * FROM tb_procourse_issue ");
+        $query = mysqli_query($conn, "SELECT * FROM tb_procourse_issue WHERE stu_matric='$stu_matric'");
 
         $read_data = array();
 
@@ -47,8 +41,9 @@
 
     }elseif($postjson['action'] == 'issue_detail') {
     	$id=$postjson['id'];
-
-        $query = mysqli_query($conn, "SELECT * FROM tb_procourse_issue WHERE issue_id='$id'");
+        // $stu_matric=$postjson['student'];
+        $query = mysqli_query($conn, "SELECT i.*,s.* FROM tb_procourse_issue i 
+        LEFT JOIN tb_student s ON s.student_matric=i.stu_matric WHERE issue_id='$id'");
 
         $read_data = array();
 
@@ -56,6 +51,8 @@
             $data = array(
                 'issue_id' => $read['issue_id'],
                 'issue_details' => $read['issue_details'],
+                'stu_matric' => $read['stu_matric'],
+                'student_name' => $read['student_name'],
                 'issue_title' => $read['issue_title'],
                 'issue_status' => $read['issue_status'],
                 'issue_answer' => $read['issue_answer'],          
@@ -82,8 +79,8 @@
 
     }elseif($postjson['action'] == 'edit_issue') {
     	$sid=$postjson['sid'];
-
-        $update = mysqli_query($conn, "UPDATE tb_procourse_issue SET stu_matric = '$postjson[matric]', issue_title = '$postjson[title]', issue_details = '$postjson[content]', issue_date = '$today', issue_status = '0' WHERE `tb_procourse_issue`.`issue_id` = '$sid'");
+        $stu_matric=$postjson['matric'];
+        $update = mysqli_query($conn, "UPDATE tb_procourse_issue SET stu_matric = '$stu_matric', issue_title = '$postjson[title]', issue_details = '$postjson[content]', issue_date = '$today', issue_status = '0' WHERE `tb_procourse_issue`.`issue_id` = '$sid'");
 
         if($update) {
 			$result = json_encode(array('success'=>true, 'msg'=>'success'));
