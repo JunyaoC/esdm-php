d808a4b257a1ad5472c6549af12c9cfc05d05bbf
atikahhanip <atikahhanip@gmail.com>
2021-05-23 21:21:50 +0800

    update complaint-form.php

--- php-folder/hostel/complaint-form.php
+++ php-folder/hostel/complaint-form.php
@@ -3,7 +3,7 @@
 	include "../connect.php";
 	$postjson = json_decode(file_get_contents('php://input'), true);
 
-	if($postjson['action'] == 'list_status') {
+	/*if($postjson['action'] == 'list_status') {
 
 		$query = mysqli_query($conn, "SELECT * FROM tb_status");
 		$read_status = array();
@@ -19,13 +19,17 @@
 
 	    $result = json_encode(array('success'=>true,'msg'=>'success','stats'=>$read_status));
 	    echo $result;
-	}
+	}*/
 
 	    if($postjson['action'] == 'select_status'){
 	    	$today = date('Y-m-d');
 
 	    	//$insert = mysqli_query($conn,"INSERT INTO tb_hostel_reg SET hostel_id = '$postjson[kolej_id]',student_id = '$postjson[matric]',reg_date = '$today',reg_status ='Pending',reg_phase= 2");
-			$insert = mysqli_query($conn,"INSERT INTO tb_hos_complaint SET student_id = '$postjson[matric]',complaint_reason = '$postjson[reason]',permission = '$postjson[status_id]',complaint_date = '$today',complaint_status ='Pending'");
+			//$insert = mysqli_query($conn,"INSERT INTO tb_hos_complaint SET student_id = '$postjson[matric]',complaint_reason = '$postjson[reason]',permission = '$postjson[status_id]',complaint_date = '$today',complaint_status ='Pending'");
+
+
+			$insert = mysqli_query($conn,"INSERT INTO tb_hos_complaint SET student_id = '$postjson[matric]',complaint_reason = '$postjson[reason]',
+			permission = '$postjson[permissions]',complaint_date = '$today',complaint_status ='Pending'");
 	    	
 	    	if($insert){
 	    		$result = json_encode(array('success'=>true,'msg'=>'success'));
