531889ce8113c38405e42017708d8494cbd42c98
unknown <hun_ho@hotmail.com>
2021-06-01 22:27:30 +0800

    Update control-register.php

--- php-folder/hostel/control-register.php
+++ php-folder/hostel/control-register.php
@@ -2,7 +2,7 @@
     include "../connect.php";
 	$postjson = json_decode(file_get_contents('php://input'), true);
 
-    // To control the kuota pengetua
+    // Kuota pengetua: To control the kuota pengetua
     if($postjson['action'] == 'check-register'){
         $query = mysqli_query($conn, "SELECT COUNT(*) FROM tb_hostel_reg WHERE student_id='A18CS1234' AND reg_phase = 1 ");
 		
@@ -21,9 +21,66 @@
 	    echo $result;
     }
 
-    // To control the open-registration
-    if($postjson['action'] == 'checking'){
+	// Kuota Pengetua: To control the kuota pengetua
+    if($postjson['action'] == 'check_kpstatus'){
+        $query = mysqli_query($conn, "SELECT * FROM tb_hostel_reg WHERE student_id='A18CS1234' AND reg_phase = 1 ");
+		
+		$read_data = array();
+
+		while($read = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+			
+				$data = array(
+					'status' => $read['reg_status'],
+			   );
+				   array_push($read_data,$data);			
+	    }
+
+	    $result = json_encode(array('success'=>true,'msg'=>'success','detail'=>$read_data));
+		//echo $postjson[qty_dryer];
+	    echo $result;
+    }
+
+	// Open Registration: First check whether the student have registered the kuota pengetua
+/*	if($postjson['action'] == 'check_kp'){
         $query = mysqli_query($conn, "SELECT * FROM tb_hostel_reg WHERE student_id='A18CS1234' AND reg_phase = 1");
+		
+		$read_data = array();
+
+		while($read = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+			
+				$data = array(
+					'status' => $read['reg_status'],
+			   );
+				   array_push($read_data,$data);			
+	    }
+
+	    $result = json_encode(array('success'=>true,'msg'=>'success','detail'=>$read_data));
+		//echo $postjson[qty_dryer];
+	    echo $result;
+    }*/
+
+	// Open Registration: To control the open-registration
+	if($postjson['action'] == 'check_op'){
+        $query = mysqli_query($conn, "SELECT COUNT(*) FROM tb_hostel_reg WHERE student_id='A18CS1234' AND reg_phase = 2 ");
+		
+		$read_data = array();
+
+		while($read = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+			
+				$data = array(
+					'registered' => $read['COUNT(*)'],
+			   );
+				   array_push($read_data,$data);			
+	    }
+
+	    $result = json_encode(array('success'=>true,'msg'=>'success','detail'=>$read_data));
+		//echo $postjson[qty_dryer];
+	    echo $result;
+    }
+
+    // Open Registration: To control the open-registration
+    if($postjson['action'] == 'checking'){
+        $query = mysqli_query($conn, "SELECT * FROM tb_hostel_reg WHERE student_id='A18CS1234' AND reg_phase = 2");
 		
 		$read_data = array();
 
