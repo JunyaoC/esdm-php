62b154ad29f03bf82c17de2e2abd26fd4d9f7224
Khoo Jie Xuan <coolkhoo547@gmail.com>
2021-06-12 21:08:23 +0800

    Dashboard IMG

--- admin/admin/dashboard.php
+++ admin/admin/dashboard.php
@@ -6,6 +6,79 @@
 
 <!DOCTYPE html>
 <html lang="en">
+<head>
+  <style>
+* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {display: none;}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+</style>
+</head>
 <?php 
   include '../pages-styling.php';
 ?>
@@ -24,7 +97,7 @@
       <div class="sidebar-wrapper">
         <ul class="nav">
           <li>
-            <a class="active">
+            <a  >
               <i class="fa fa-bars"></i>
               <p>Dashboard</p>
             </a>
@@ -140,9 +213,38 @@
       </nav>
 
 
+  <br> <br> <br> <br> <br>
 
+    <div class="container">
+      <div class="slideshow-container">
 
+      <div class="mySlides fade">
+        <div class="numbertext">1 / 3</div>
+        <img src="https://media.glassdoor.com/l/a0/6e/5a/b7/main-gate-utm.jpg" style="width:100%">
+   
+      </div>
 
+      <div class="mySlides fade">
+        <div class="numbertext">2 / 3</div>
+        <img src="https://image.isu.pub/190417021944-deb7b0221e48357ca17ee6a2a9f44d6b/jpg/page_1.jpg" style="width:100%">
+        
+      </div>
+
+      <div class="mySlides fade">
+        <div class="numbertext">3 / 3</div>
+        <img src="https://mma.prnasia.com/media2/1310639/utm_graduates.jpg" style="width:100%">
+       
+      </div>
+
+      </div>
+      <br>
+
+      <div style="text-align:center">
+        <span class="dot"></span> 
+        <span class="dot"></span> 
+        <span class="dot"></span> 
+      </div>
+    </div>
 
 
 
@@ -183,6 +285,29 @@
   <script src="../js/plugins/bootstrap-notify.js"></script>
   <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="../js/paper-dashboard.min.js?v=2.0.0" type="text/javascript"></script>
+
+    <script>
+  var slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+  </script>
+
 </body>
 
 </html>
